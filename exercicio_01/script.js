223abd30bda6eebfa1c7f093bacc7a305b9a03c0
Maria <marialuizaprado13@gmail.com>
2024-12-20 11:00:31 -0300

    first commit

---
+++ exercicio_01/script.js
@@ -0,0 +1,7 @@
+// Primeiro exercício: Declare duas variáveis chamadas "idade" e "peso", atribua valores a elas e imprima os valores no console. 
+
+idade = 19;
+peso = 56;
+
+console.log("Idade: " + idade);
+console.log("Peso: " + peso);
---
+++ exercicio_02/script.js
@@ -0,0 +1,5 @@
+// Declare uma constante chamada "PI", atribua o valor de Pi a ela e imprima o valor no console.
+
+const PI = 3.14159;
+
+console.log("O valor de Pi é: " + PI); 
